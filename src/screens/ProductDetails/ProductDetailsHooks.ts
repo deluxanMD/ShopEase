bd920a6b93d455f82a6add00bef6de9e47ad3421
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2025-04-03 09:43:16 +0530

    Add Images horizontal carousel on product details screen

---
+++ src/screens/ProductDetails/ProductDetailsHooks.ts
@@ -0,0 +1,49 @@
+import {useEffect, useState} from 'react';
+import {Alert} from 'react-native';
+
+import axios from 'axios';
+
+import {Product} from '../../types/ProductsTypes';
+
+export const useProductDetails = (productId: number) => {
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const errorAlert = (error: string) => {
+    Alert.alert('Network Error', error, [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+    ]);
+  };
+
+  const getProductById = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(
+        `${process.env.API_URL}/products/${productId}`,
+      );
+
+      if (response.status !== 200) {
+        errorAlert('Something went wrong! Please try again later.');
+      }
+
+      setProduct(response.data);
+    } catch (_error) {
+      console.log(_error);
+      errorAlert('Something went wrong! Please try again later.');
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getProductById();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [productId]);
+
+  return {product, loading};
+};
--- src/screens/ProductDetails/ProductDetailsScreen.tsx
+++ src/screens/ProductDetails/ProductDetailsScreen.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 
 import {RouteProp, useRoute} from '@react-navigation/native';
@@ -6,15 +6,20 @@ import {RouteProp, useRoute} from '@react-navigation/native';
 import {RootStackParamList} from '../../navigations/StackNavigation';
 import DetailsHeader from '../../components/DetailsHeader/DetailsHeader';
 import styles from './ProductDetailsScreenStyles';
+import {useProductDetails} from './ProductDetailsHooks';
+import ProductImages from '../../components/ProductImages/ProductImages';
 
 const ProductDetailsScreen = () => {
   const {
     params: {productId},
   } = useRoute<RouteProp<RootStackParamList, 'ProductDetails'>>();
 
+  const {product, loading} = useProductDetails(productId);
+
   return (
     <View style={styles.container}>
       <DetailsHeader />
+      <ProductImages images={product?.images || []} />
     </View>
   );
 };
